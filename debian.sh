ca4194225a9d0a5bc985bdfb9eacb70a94859a3f
idk <idk@idk.local>
2016-03-26 14:34:37 -0400

    first version

---
+++ debian.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+# Configure your paths and filenames
+SOURCEBINPATH="usr/bin/"
+SOURCEBIN="srd"
+SOURCEDOC="README.md"
+DEBFOLDER="simple-repo-debigger"
+SOURCEDOCPATH="usr/share/doc/$DEBFOLDER"
+DEBVERSION=$(date +%Y%m%d)
+
+cd "$DEBFOLDER"
+
+git pull origin master
+
+DEBFOLDERNAME="../$DEBFOLDER-$DEBVERSION"
+
+# Create your scripts source dir
+mkdir $DEBFOLDERNAME
+
+# Copy your script to the source dir
+cp $SOURCEBINPATH $DEBFOLDERNAME -R
+cd $DEBFOLDERNAME
+
+# Create the packaging skeleton (debian/*)
+dh_make -s --indep --createorig -p "$DEBFOLDER-$DEBVERSION"
+
+mkdir -p debian/tmp
+cp usr debian/tmp/usr -R
+
+# Remove make calls
+grep -v makefile debian/rules > debian/rules.new 
+mv debian/rules.new debian/rules 
+
+# debian/install must contain the list of scripts to install 
+# as well as the target directory
+echo "$SOURCEBINPATH$SOURCEBIN" usr/bin > debian/install 
+echo "$SOURCEDOCPATH$SOURCEDOC" usr/share/doc/$DEBFOLDER >> debian/install
+
+# Remove the example files
+rm debian/*.ex
+
+# Build the package.
+# You  will get a lot of warnings and ../somescripts_0.1-1_i386.deb
+debuild -us -uc > ../log 
---
+++ usr/share/doc/simple-repo-debigger/README.md
@@ -0,0 +1,2 @@
+# simple-repo-debigger
+A simple shell script to find all files larger than 100 MB and remove them from the git repository completely.
